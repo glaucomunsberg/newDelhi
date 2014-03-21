fd6ab512897c4fbf39c00b1a1a4583f05bb188fa
Glauco Robero Munsberg dos Santos <glaucomunsberg@gmail.com>
2014-03-21 19:15:54 -0300

    Cleaning a a old structure

---
+++ api/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Api::Application.config.secret_key_base = '0d23982362ac06bc1a36384255c112699c9b02ac519fd0a4e855e1e8fc153af69cf6edc741789b52929d01f143b96e257aa46bdbd576b21db605eda6a36b8bcb'
