82d908abaa20fb252df9ebaec6f14f9264c85577
Glauco Robero Munsberg dos Santos <glaucomunsberg@gmail.com>
2014-03-21 19:17:09 -0300

    Cleaning a a old structure fase 2

--- README.md
+++
@@ -1,4 +0,0 @@
-newDelhi
-========
-
-NewDalhi is the API used by Apps's Plataforma Paraformal to see and send new paraformalidades. About Apps see Mumbai.
--- config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'f1e65146ab437957a719e727afd4edcfaf724354c504390b88b40e030c741ff84221c817b93d1ce0ef6512865ce8fdb4c1e831e3018b6dcc557b45bd0c279ce4';
